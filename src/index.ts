c72af7ab8beee7c3f5e3f7e790d91a3f843d8482
chethanck <chethanckm66@gmail.com>
2022-01-18 06:56:28 +0530

    sorting using Linked List

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumberCollection } from './NumberCollection';
 import { CharacterCollection } from './CharacterCollection';
+import { LinkedList } from './LinkedList';
 
 const numberCollection = new NumberCollection([5, 3, -2, 10, 2]);
 const sorter = new Sorter(numberCollection);
@@ -10,4 +11,14 @@ console.log(numberCollection.data);
 const charCollections = new CharacterCollection('Xaayb');
 const sortCharacters = new Sorter(charCollections);
 sortCharacters.sort();
-console.log(charCollections);
+console.log(charCollections.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sortList = new Sorter(linkedList);
+sortList.sort();
+linkedList.print();
